6dc3411f564fdd3846c08260f947ca9b044d01a5
alexantosh <alexantosh@yahoo.com>
2016-01-17 18:45:00 +0200

    Disturbingly re-executes!

--- Network/src/DrawNetwork.java
+++ Network/src/DrawNetwork.java
@@ -2,13 +2,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.util.Random;
-
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -20,13 +23,30 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
 
+import Algorithms.LexDraw;
 import Algorithms.Point;
-import Algorithms.Draw;
 
 public class DrawNetwork extends JPanel 
 {
 	String feedback="";
 	int met = 0;
+	int selected; 
+	public static final Color		WHITE				= Color.WHITE;
+	BufferedImage offscreenImage,  onscreenImage;
+	Graphics2D offscreen, onscreen;
+	private static final Color		DEFAULT_CLEAR_COLOR	= WHITE;
+	
+	private boolean					defer				= false;
+	private Color					penColor;
+	private double					xmin, ymin, xmax, ymax;
+	
+	// boundary of drawing canvas, 0% border
+	private static final double		BORDER				= 0.0;
+	private static final double		DEFAULT_XMIN		= 0.0;
+	private static final double		DEFAULT_XMAX		= 1.0;
+	private static final double		DEFAULT_YMIN		= 0.0;
+	private static final double		DEFAULT_YMAX		= 1.0;
+	
 	Color color;
 	JPanel labelFields = new JPanel(new BorderLayout(2, 3));
 	JPanel networkField = new JPanel();
@@ -97,11 +117,17 @@ public class DrawNetwork extends JPanel
 	public void init()
 	{
 		
-		setfHeight( 900 );
-		setfWidth( 700 );
+		setfHeight( 750 );
+		setfWidth( 500 );
+		//setSize(getfWidth(), getfWidth());
+		//Initialising those buffered images
+		offscreenImage = new BufferedImage( getfWidth(), getfHeight(), BufferedImage.TYPE_INT_ARGB); //width and height
+		onscreenImage = new BufferedImage( getfWidth(), getfHeight(), BufferedImage.TYPE_INT_ARGB);
+		offscreen = offscreenImage.createGraphics();
+		onscreen = onscreenImage.createGraphics();
 //		frame = new JFrame();
 //		frame.setLayout( new BorderLayout(3,3));
-		
+//		
 		color = new Color(1.0f, 1.0f, 0.0f, 0.7f);
 		
 		//Handle the algorithms
@@ -114,13 +140,13 @@ public class DrawNetwork extends JPanel
 			{
 				if( e.getStateChange() == ItemEvent.SELECTED )
 				{
-					int selected = algos.getSelectedIndex(); 
+					selected = algos.getSelectedIndex(); 
 					if( selected == 0 )
 					{
 						setMet( selected );
 						feedback += "\nGo Random. Selected: Random==" + algorithms[selected];
 						progressInfoTextField.setText( feedback );
-						repaint();
+//						repaint();
 					}
 					// Set to Voronoi
 					else if( selected == 1 )
@@ -129,21 +155,21 @@ public class DrawNetwork extends JPanel
 						feedback += "\nGo Voronoi. Selected: Voronoi==" + algorithms[selected];
 						progressInfoTextField.setText( feedback );
 						//drawVoronoi( g );
-						repaint();
+//						repaint();
 					}
 					else if( selected == 2 )
 					{
 						setMet( selected );
 						feedback += "\nGo LEACH. Selected: LEACH==" + algorithms[selected];
 						progressInfoTextField.setText( feedback );
-						repaint();
+//						repaint();
 					}
 					else 
 					{
 						setMet( selected );
 						feedback += "\nGo Us. Selected: Ours==" + algorithms[selected];
 						progressInfoTextField.setText( feedback );
-						repaint();
+//						repaint();
 					}
 				}
 			}
@@ -212,7 +238,7 @@ public class DrawNetwork extends JPanel
 		gui.add( labelFields ); // changed From BorderLayout.NORTH AND SOUTH. To  GridLayout For Even Distribution of components
 		gui.add( guiCenter );
 		this.setBorder( new TitledBorder("Network"));
-//		this.setBackground( Color.BLUE );                     //Set panel Color
+		this.setBackground( DEFAULT_CLEAR_COLOR );                     //Set panel Color
 //		frame.add( this, BorderLayout.CENTER );
 //		frame.add( gui, BorderLayout.EAST );
 //		frame.setSize( getfHeight(),  getfWidth() );
@@ -253,60 +279,90 @@ public class DrawNetwork extends JPanel
 	
 	public void drawVoronoi( Graphics g ) // get Graphics later
 	{
+		System.out.println("Closing the JFrame of test by force");
+		//Test.frame.dispose();
+		
+		//LexDraw draw = new LexDraw();
+		//draw.setCanvasSize(getfWidth(), getfHeight());
+		offscreen = offscreenImage.createGraphics();
+		onscreen = onscreenImage.createGraphics();
+		offscreen.setColor(DEFAULT_CLEAR_COLOR);
+		offscreen.fillRect(0, 0, getfWidth(), getfHeight());
+		this.setXscale(0, getfWidth());
+		this.setYscale(0, getfHeight());
+		this.show(20);
+		System.out.println("In drawVoronoi now");
 		//populateNodes( getN(), getFovAngle() );
 		// initialise network to size of the Network
-		g.setColor( Color.YELLOW );
-		for( int i= 0; i < nodes.length; i++ )
-			drawNode(g, nodes[i].getxCoor(), nodes[i].getyCoor(), nodes[i].getsRange(), this.sRange, nodes[i].getStartAngle(), nodes[i].getFovAngle() );
-		Point[][] nearest = new Point[ getWidth()][getHeight()];
-		Point q = new Point(1,3);
-		//from rows
-		Point p = new  Point( nodes[0].getxCoor(), nodes[0].getyCoor() );
-		for( int i = 0; i < getWidth(); i++ )
+		//g.setColor( Color.YELLOW );
+//		for( int i= 0; i < nodes.length; i++ )
+//			drawNode(g, nodes[i].getxCoor(), nodes[i].getyCoor(), nodes[i].getsRange(), this.sRange, nodes[i].getStartAngle(), nodes[i].getFovAngle() );
+		Point[][] nearest = new Point[ getfWidth()][getfHeight()];
+		for (int k = 0; k < nodes.length; k++)
 		{
-			for(int j=0; j<getHeight(); j++ )
+			//from rows
+			Point p = new Point(nodes[k].getxCoor(), nodes[k].getyCoor());
+			System.out.println("Inserting:       " + p);
+			setPenColor(Color.getHSBColor((float) Math.random(), .7f, .7f));
+			for (int i = 0; i < getfWidth(); i++)
 			{
-				if( j < nodes.length - 1 )
+				for (int j = 0; j < getfHeight(); j++)
 				{
-					q = new Point( nodes[j+1].getxCoor(), nodes[j+1].getyCoor() );
-					if (i < nodes.length )
+					System.out.println("Creating p the: "+i+" th");
+						Point q = new Point(i,j);
 						if ((nearest[i][j] == null) || (q.distanceTo(p) < q.distanceTo(nearest[i][j])))
 						{
-							nearest[i][j] = new Point(nodes[i].getxCoor(),nodes[i].getyCoor());
-							this.color = new Color(0.3f, 0.2f, 0.1f, 0.23f );
-							g.setColor(this.color);
-							g.fillRect(nodes[i].getxCoor() + 1, nodes[j].getxCoor() + 1, gettRange(), gettRange() );  //draw.filledSquare
+							nearest[i][j] = p;
+							this.color = new Color((float) Math.random(), (float) Math.random(),
+							        (float) Math.random(), .5f);
+							//g.setColor(this.color);
+							//g.fillRect(i + 1, j + 1, gettRange(), gettRange()); //draw.filledSquare
+							System.out.println("drawing square now");
+							this.square(i + 0.5, j + 0.5, 0.5);
 						}
+						System.out.println("Outside if in 2nd for");
 				}
-			}
+			} 
+			this.setPenColor(Color.BLACK);
+			this.filledCircle( nodes[k].getxCoor(), nodes[k].getyCoor(), 4);
+			//g.setColor( Color.YELLOW );
+			//drawNode(g, nodes[k].getxCoor(), nodes[k].getyCoor(), nodes[k].getsRange(), this.sRange, nodes[k].getStartAngle(), nodes[k].getFovAngle() );
 		}
 		
+		this.show(20);
+		g.drawImage(offscreenImage, 0, 0, null);
+		
+		System.out.println("Done voronoing");
+		return;
+		
 	}
+	
 	
 	// put nodes onto panel
 	public void paintComponent( Graphics g)
 	{
 		super.paintComponent( g );
-		int todo = getMet();
-		switch( todo )
-		{
-			case 0:
-				drawRandom( g );
-				System.out.println("Deployed Randomly");
-				break;
-			case 1:
-				drawVoronoi( g );
-				System.out.println("Voronoi working now...");
-				break;
-			case 2:
-				System.out.println("Leach");
-				break;
-			case 3:
-				System.out.println("Ours");
-				break;
-				default:
-					drawRandom( g );
-		}
+		drawRandom( g );
+		//int todo = getMet();
+//		switch( todo )
+//		{
+//			case 0:
+//				System.out.println("Deployed Randomly");
+//				drawRandom( g );				
+//				break;
+//			case 1:
+//				System.out.println("Voronoi working now...");
+//				drawVoronoi( g );
+//				break;
+//			case 2:
+//				System.out.println("Leach");
+//				break;
+//			case 3:
+//				System.out.println("Ours");
+//				break;
+//				default:
+//					drawRandom( g );
+//		}
 		
 	}
 	
@@ -333,6 +389,13 @@ public class DrawNetwork extends JPanel
 		setFovAngle( fov );
 		populateNodes();
 	}
+	
+	public void setPenColor(Color color)
+	{
+		penColor = color;
+		offscreen.setColor(penColor);
+	}
+	
 	public int getfHeight()
 	{
 		return fHeight;
@@ -411,4 +474,197 @@ public class DrawNetwork extends JPanel
 	{
 		this.n = n;
 	}
+	
+	/***************************************************************************
+	 * User and screen coordinate systems.
+	 ***************************************************************************/
+
+	/**
+	 * Sets the x-scale to be the default (between 0.0 and 1.0).
+	 */
+	public void setXscale()
+	{
+		setXscale(DEFAULT_XMIN, DEFAULT_XMAX);
+	}
+
+	/**
+	 * Sets the y-scale to be the default (between 0.0 and 1.0).
+	 */
+	public void setYscale()
+	{
+		setYscale(DEFAULT_YMIN, DEFAULT_YMAX);
+	}
+
+	/**
+	 * Sets the x-scale.
+	 *
+	 * @param min
+	 *            the minimum value of the x-scale
+	 * @param max
+	 *            the maximum value of the x-scale
+	 */
+	public void setXscale(double min, double max)
+	{
+		double size = max - min;
+		xmin = min - BORDER * size;
+		xmax = max + BORDER * size;
+	}
+
+	/**
+	 * Sets the y-scale.
+	 *
+	 * @param min
+	 *            the minimum value of the y-scale
+	 * @param max
+	 *            the maximum value of the y-scale
+	 */
+	public void setYscale(double min, double max)
+	{
+		double size = max - min;
+		ymin = min - BORDER * size;
+		ymax = max + BORDER * size;
+	}
+
+	// helper functions that scale from user coordinates to screen coordinates
+	// and back
+	private double scaleX(double x)
+	{
+		return getWidth() * (x - xmin) / (xmax - xmin);
+	}
+
+	private double scaleY(double y)
+	{
+		return getHeight() * (ymax - y) / (ymax - ymin);
+	}
+
+	private double factorX(double w)
+	{
+		return w * getWidth() / Math.abs(xmax - xmin);
+	}
+
+	private double factorY(double h)
+	{
+		return h * getHeight() / Math.abs(ymax - ymin);
+	}
+
+	private double userX(double x)
+	{
+		return xmin + x * (xmax - xmin) / getWidth();
+	}
+
+	private double userY(double y)
+	{
+		return ymax - y * (ymax - ymin) / getHeight();
+	}
+	
+	private void draw()
+	{
+		if (defer)
+			return;
+		onscreen.drawImage(offscreenImage, 0, 0, null);
+		repaint();
+	}
+	
+	public void show(int t)
+	{
+		defer = false;
+		draw();
+		try
+		{
+			Thread.sleep(t);
+		} catch (InterruptedException e)
+		{
+			System.out.println("Error sleeping");
+		}
+		defer = true;
+	}
+	
+	private void pixel(double x, double y)
+	{
+		offscreen.fillRect((int) Math.round(scaleX(x)), (int) Math.round(scaleY(y)), 1, 1);
+	}
+	
+	public int getSelected()
+	{
+		return selected;
+	}
+
+	public void setSelected(int selected)
+	{
+		this.selected = selected;
+	}
+	
+	public void square(double x, double y, double r)
+	{
+		if (r < 0)
+			throw new IllegalArgumentException("square side length can't be negative");
+		double xs = scaleX(x);
+		double ys = scaleY(y);
+		double ws = factorX(2 * r);
+		double hs = factorY(2 * r);
+		if (ws <= 1 && hs <= 1)
+			pixel(x, y);
+		else
+			offscreen.draw(new Rectangle2D.Double(xs - ws / 2, ys - hs / 2, ws, hs));
+		draw();
+	}
+	
+	/**
+	 * Draws a filled square of side length 2r, centered on (x, y).
+	 *
+	 * @param x
+	 *            the x-coordinate of the center of the square
+	 * @param y
+	 *            the y-coordinate of the center of the square
+	 * @param r
+	 *            radius is half the length of any side of the square
+	 * @throws IllegalArgumentException
+	 *             if r is negative
+	 */
+	public void filledSquare(double x, double y, double r)
+	{
+		if (r < 0)
+			throw new IllegalArgumentException("square side length can't be negative");
+		double xs = scaleX(x);
+		double ys = scaleY(y);
+		double ws = factorX(2 * r);
+		double hs = factorY(2 * r);
+		if (ws <= 1 && hs <= 1)
+			pixel(x, y);
+		else
+			offscreen.fill(new Rectangle2D.Double(xs - ws / 2, ys - hs / 2, ws, hs));
+		draw();
+	}
+	//circles
+	public void circle(double x, double y, double r)
+	{
+		if (r < 0)
+			throw new IllegalArgumentException("circle radius can't be negative");
+		double xs = scaleX(x);
+		double ys = scaleY(y);
+		double ws = factorX(2 * r);
+		double hs = factorY(2 * r);
+		if (ws <= 1 && hs <= 1)
+			pixel(x, y);
+		else
+			offscreen.draw(new Ellipse2D.Double(xs - ws / 2, ys - hs / 2, ws, hs));
+		draw();
+	}
+	
+	public void filledCircle(double x, double y, double r)
+	{
+		if (r < 0)
+			throw new IllegalArgumentException("circle radius can't be negative");
+		double xs = scaleX(x);
+		double ys = scaleY(y);
+		double ws = factorX(2 * r);
+		double hs = factorY(2 * r);
+		if (ws <= 1 && hs <= 1)
+			pixel(x, y);
+		else
+			offscreen.fill(new Ellipse2D.Double(xs - ws / 2, ys - hs / 2, ws, hs));
+		draw();
+	}
+	
+	
 }
--- Network/src/Test.java
+++ Network/src/Test.java
@@ -4,16 +4,19 @@ import javax.swing.JFrame;
 
 public class Test
 {
-	public static void main(String[] args)
+	public Test()
 	{
 		DrawNetwork netPanel = new DrawNetwork( 10, 65  );
-		JFrame frame;
-		frame = new JFrame();
 		frame.setLayout( new BorderLayout(3,3));
 		frame.add( netPanel, BorderLayout.CENTER );
 		frame.add( netPanel.gui, BorderLayout.EAST );
 		frame.setSize( netPanel.getfHeight(),  netPanel.getfWidth() );
 		frame.setVisible( true );
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	}
+	static JFrame frame = new JFrame();
+	public static void main(String[] args)
+	{
+		Test tst = new Test();
 	}
 }
